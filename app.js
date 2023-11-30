40754711bd37ba65d18e2be54da650a10ff544da
sanjanagb <sanjana051198@gmail.com>
2023-11-29 21:23:10 -0500

    Assignment 6 - Connect to DB

--- app.js
+++ app.js
@@ -4,16 +4,39 @@ import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-import cors from "cors";
 import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
+import cors from "cors";
+import session from "express-session";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+                 credentials: true,
+                 origin: process.env.FRONTEND_URL
+             }));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Lab5(app);
 HelloRoutes(app);
 
